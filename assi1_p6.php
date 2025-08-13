a721c5e54bbb338e3002a17cacc99d100ef9bd9f
Krn30 <karanudani0@gmail.com>
2025-08-13 11:13:08 +0530

    Update assi1_p6.php

--- assi1_p6.php
+++ assi1_p6.php
@@ -1,48 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Array Operations</title>
-</head>
-<body>
-
-<form method="post">
-    Enter first array values (comma separated): 
-    <input type="text" name="arr1"><br><br>
-
-    Enter second array values (comma separated): 
-    <input type="text" name="arr2"><br><br>
-
-    <input type="submit" name="submit" value="Perform Operations">
-</form>
-
 <?php
-if (isset($_POST['submit'])) {
-    // Convert user input into arrays
-    $arr1 = explode(",", str_replace(" ", "", $_POST['arr1']));
-    $arr2 = explode(",", str_replace(" ", "", $_POST['arr2']));
 
-    // a) Print the values of array
-    echo "<h3>a) Array 1:</h3>";
-    print_r($arr1);
-
-    // b) Reverse an array
-    echo "<h3>b) Reverse of Array 1:</h3>";
-    print_r(array_reverse($arr1));
-
-    // c) Merge two arrays in sorted manner
-    $merged = array_merge($arr1, $arr2);
-    sort($merged);
-    echo "<h3>c) Merged & Sorted Array:</h3>";
-    print_r($merged);
-
-    // d) Add values of all elements of an array
-    // Convert all to numbers before summing
-    $numericArray = array_map('floatval', $arr1);
-    $sum = array_sum($numericArray);
-    echo "<h3>d) Sum of elements in Array 1:</h3>";
-    echo $sum;
+echo "<h2>Printing numbers 15 to 20 using WHILE loop:</h2>";
+$num = 15;
+while ($num <= 20) {
+    echo $num . " ";
+    $num++;
 }
+
+echo "<br><br>";
+
+echo "<h2>Printing numbers 15 to 20 using DO-WHILE loop:</h2>";
+$num = 15;
+do {
+    echo $num . " ";
+    $num++;
+} while ($num <= 20);
+
 ?>
 
-</body>
-</html>
